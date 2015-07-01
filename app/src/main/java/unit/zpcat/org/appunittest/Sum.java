11f0905791521a6121d6dd96287f9f93faa3ab2f
suzp1984 <suzp1984@gmail.com>
2015-07-01 09:24:45 +0800

    make junit test works with android.jar

--- app/src/main/java/unit/zpcat/org/appunittest/Sum.java
+++ app/src/main/java/unit/zpcat/org/appunittest/Sum.java
@@ -1,11 +1,15 @@
 package unit.zpcat.org.appunittest;
 
+import android.util.Log;
+
 /**
  * Created by moses on 6/30/15.
  */
 public class Sum {
+    private final String TAG = "Sum";
 
     public int add(int a, int b) {
+        Log.e(TAG, "Sum.add(" + a + ", " + b + ");");
         return a + b;
     }
 }
